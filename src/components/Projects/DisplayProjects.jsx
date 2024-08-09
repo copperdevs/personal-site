a574428175d58877c50d02ac85faefadbd1d94a3
copper <79734708+copperdevs@users.noreply.github.com>
2024-08-09 17:21:24 -0500

    animate on scroll

--- src/components/Projects/DisplayProjects.jsx
+++ src/components/Projects/DisplayProjects.jsx
@@ -4,21 +4,23 @@ const ProjectList = ({ projects, showCategoryIcon }) => (
   <section>
     {projects.map((project, index) => (
       <>
-        <ProjectItem
-          title={project.data.title}
-          description={project.data.description}
-          url={project.data.link}
-          source={project.data.source}
-          category={project.data.category}
-          showCategoryIcon={showCategoryIcon}
-        />
-        {index < projects.length - 1 && <hr />}
-        {!(index < projects.length - 1) && (
-          <>
-            <br />
-            <br />
-          </>
-        )}
+        <section data-aos="zoom-in">
+          <ProjectItem
+            title={project.data.title}
+            description={project.data.description}
+            url={project.data.link}
+            source={project.data.source}
+            category={project.data.category}
+            showCategoryIcon={showCategoryIcon}
+          />
+          {index < projects.length - 1 && <hr />}
+          {!(index < projects.length - 1) && (
+            <>
+              <br />
+              <br />
+            </>
+          )}
+        </section>
       </>
     ))}
   </section>
