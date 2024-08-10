55a4144c1601eaeadbc56a25cd22a8228f29ba58
copper <79734708+copperdevs@users.noreply.github.com>
2024-08-09 23:15:44 -0500

    callout and api

--- src/lib/github.ts
+++ src/lib/github.ts
@@ -1,17 +1,35 @@
 export async function getMostRecentCommit(repo: string) {
   const response = await fetch(`https://api.github.com/repos/${repo}/commits`);
+
+  if (!response.ok) {
+    console.log("Github Recent Commit | Network response not okay");
+    return null;
+  }
+
   const data = await response.json();
   return data[0];
 }
 
 export async function getMostRecentCommitName(repo: string) {
   const response = await fetch(`https://api.github.com/repos/${repo}/commits`);
+
+  if (!response.ok) {
+    console.log("Github Recent Commit Name | Network response not okay");
+    return "couldn't get recent commit name";
+  }
+
   const data = await response.json();
   return data[0].commit.message;
 }
 
 export async function getMostRecentCommitUrl(repo: string) {
-    const response = await fetch(`https://api.github.com/repos/${repo}/commits`);
-    const data = await response.json();
-    return data[0].html_url;
+  const response = await fetch(`https://api.github.com/repos/${repo}/commits`);
+
+  if (!response.ok) {
+    console.log("Github Recent Commit Url | Network response not okay");
+    return "##";
+  }
+
+  const data = await response.json();
+  return data[0].html_url;
 }
