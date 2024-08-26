c9259b8a958ab5d3507eff52a88fb9de89ebb016
copper <79734708+copperdevs@users.noreply.github.com>
2024-08-25 19:18:15 -0500

    lanyard can be a later thing

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,14 +1,15 @@
-
 export enum Status {
   Online = "green-glow",
   DoNotDisturb = "red-glow",
   Idle = "yellow-glow",
   Offline = "gray-glow",
+  None = "",
 }
 
-export enum DiscordStatus{
+export enum DiscordStatus {
   Online = "online",
   DoNotDisturb = "dnd",
   Idle = "idle",
   Offline = "offline",
+  None = "",
 }
