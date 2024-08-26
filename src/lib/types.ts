619fb611e6f462d6dde433e26868c4cf7d1ade0a
copper <79734708+copperdevs@users.noreply.github.com>
2024-08-25 21:02:39 -0500

    icons and socials

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,3 +1,5 @@
+// i found out about typescript enums and now im having fun with them
+
 export enum Status {
   Online = "green-glow",
   DoNotDisturb = "red-glow",
@@ -12,4 +14,13 @@ export enum DiscordStatus {
   Idle = "idle",
   Offline = "offline",
   None = "",
+}
+
+export enum IconType {
+  Discord = "fa-brands fa-discord",
+  Github = "fa-brands fa-github",
+  Steam = "fa-brands fa-steam",
+  Twitch = "fa-brands fa-twitch",
+  Twitter = "fa-brands fa-twitter",
+  Youtube = "fa-brands fa-youtube",
 }
