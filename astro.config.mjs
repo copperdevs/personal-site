6e38376ea5e3a42f852a744fa7e88f30d7a3c8fb
copperdevs <79734708+copperdevs@users.noreply.github.com>
2024-10-18 18:57:54 -0500

    sitemap and robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,10 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  site: "https://copperdevs.com",
+  integrations: [react(), sitemap()],
 });
