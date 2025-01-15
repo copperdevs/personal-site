823486ed579d7e51021e1630a29d494fdeb7098a
copperdevs <79734708+copperdevs@users.noreply.github.com>
2025-01-15 14:20:46 -0600

    projects page

---
+++ src/content.config.ts
@@ -0,0 +1,13 @@
+import { defineCollection, z } from "astro:content";
+
+import { glob } from "astro/loaders";
+
+export const collections = {
+  projects: defineCollection({
+    loader: glob({ pattern: ["*.md"], base: "src/content/projects/" }),
+    schema: z.object({
+      name: z.string(),
+      description: z.string(),
+    }),
+  }),
+};
---
+++ src/content/projects/test-project.md
@@ -0,0 +1,4 @@
+---
+name: Test Project
+description: the description of the test project perchance :3
+---
