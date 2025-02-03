fd15b278961632d473447f14107f7c70bb34fa58
copperdevs <79734708+copperdevs@users.noreply.github.com>
2025-02-03 14:58:58 -0600

    socials and lists styling

--- src/variables.ts
+++ src/variables.ts
@@ -1,5 +1,8 @@
 import type { Dictionary } from "./lib/util";
 
 export const Socials: Dictionary<string, string> = {
-  twitter: "https://www.google.com/",
+  twitter: "https://x.com/copperdevs",
+  youtube: "https://www.youtube.com/@copperdevs",
+  github: "https://github.com/copperdevs",
+  bluesky: "https://bsky.app/profile/copperdevs.com",
 };
