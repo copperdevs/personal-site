41accb7ff7098719ee8efc4f38ad41b2e1032b78
copperdevs <github@copperdevs.com>
2026-02-17 01:28:20 -0600

    drop umami

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,10 @@
 import sitemap from "@astrojs/sitemap";
-import umami from "@yeskunall/astro-umami";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://copperdevs.com",
-	vite: {
-		server: {
-			watch: {
-				usePolling: true,
-			},
-		},
-	},
 	integrations: [
 		sitemap(),
-		umami({ id: "bc408a92-1357-4f9b-86ad-5f608ff9e04d" }),
 	],
 });
