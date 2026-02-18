e3cac7824de57fa6d2c89175e3d3c4cdbd6a4bc0
copperdevs <github@copperdevs.com>
2026-02-18 01:12:43 -0600

    packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://copperdevs.com",
-	vite: {
-		server: {
-			watch: {
-				usePolling: true,
-			},
-		},
-	},
-	integrations: [
-		sitemap(),
-	],
+    site: "https://copperdevs.com",
+    vite: {
+        server: {
+            watch: {
+                usePolling: true,
+            },
+        },
+    },
+    integrations: [sitemap(), preact()],
 });
