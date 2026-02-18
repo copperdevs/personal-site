009689eeb495e55eec6bf27b3b59750133126d5a
copperdevs <github@copperdevs.com>
2026-02-18 01:10:10 -0600

    layout work

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,13 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://copperdevs.com",
+	vite: {
+		server: {
+			watch: {
+				usePolling: true,
+			},
+		},
+	},
 	integrations: [
 		sitemap(),
 	],
