a233a44392fdd66cd4d6971439dad94d6150b3fb
copperdevs <github@copperdevs.com>
2026-02-19 23:39:28 -0600

    sub section work

--- src/components/current-section.tsx
+++
@@ -1,7 +0,0 @@
-import { useStore } from "@nanostores/preact";
-import { currentSection } from "../sectionStore";
-
-export function CurrentSection() {
-	const section = useStore(currentSection);
-	return <p>{section}</p>;
-}
